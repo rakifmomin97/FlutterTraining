0adc3de28756cca87a36012f1bfe4b787701b247
rakifmomin97 <rakifmomin97@gmail.com>
2021-06-08 12:04:36 +0530

    day 12 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/models/catelog.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
+import 'package:flutter_catalog/widgets/item_widget.dart';
 
 class HomePage extends StatelessWidget {
   final String name = "Rakif";
 
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(10, (index) => CatelogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -13,9 +16,13 @@ class HomePage extends StatelessWidget {
         ),
         centerTitle: true,
       ),
-      body: Center(
-        child: Container(
-          child: Text("Welcome first program by $name"),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(item: dummyList[index]);
+          },
         ),
       ),
       drawer: MyDrawer(),
