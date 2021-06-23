51137c417bf0cc25328a2e9d921e02e0b34aff92
rakifmomin97 <rakifmomin97@gmail.com>
2021-06-23 10:55:10 +0530

    Day 16 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -42,14 +42,55 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: (CatelogModel.items.length != 0) ? ListView.builder(
-          itemCount: CatelogModel.items.length,
-          itemBuilder: (context, index) {
-            return ItemWidget(item: CatelogModel.items[index]);
-          },
-        ):Center(
-          child: CircularProgressIndicator(),
-        ), 
+        child: (CatelogModel.items.length != 0)
+            ? GridView.builder(
+                itemCount: CatelogModel.items.length,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 15,
+                  mainAxisSpacing: 15,
+                ),
+                itemBuilder: (context, index) {
+                  final item = CatelogModel.items[index];
+                  return Card(
+                    clipBehavior: Clip.antiAlias,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    child: GridTile(
+                      header: Container(
+                        child: Text(
+                          item.name,
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          color: Colors.blue,
+                        ),
+                      ),
+                      child: Image.network(
+                        item.image,
+                      ),
+                      footer: Container(
+                        child: Text(
+                          item.price.toString(),
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              )
+            : Center(
+                child: CircularProgressIndicator(),
+              ),
       ),
       drawer: MyDrawer(),
     );
